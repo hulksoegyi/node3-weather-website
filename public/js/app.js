531bbb719a34eaf8d6bf329c2714e4142976d26b
Soe Naing Htet <soenaing.htet@gmail.com>
2022-01-10 23:59:57 +0800

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e)=>{
     weatherMsg.textContent = 'Loading Weather Information ...';
     const location = search.value;
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             var returnStr = '';
             if(data.error){
